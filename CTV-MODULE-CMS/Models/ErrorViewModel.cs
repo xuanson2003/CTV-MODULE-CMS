261295f07d4a1be305d4d2cfec800a0837a38eb8
Xuan Son <doxuanson12b2003@gmail.com>
2024-06-13 15:38:48 +0700

    init project

---
+++ CTV-MODULE-CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CTV_MODULE_CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
