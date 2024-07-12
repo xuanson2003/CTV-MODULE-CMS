598e08c35ff25707916661c151e803ae3a7df885
Xuan Son <doxuanson12b2003@gmail.com>
2024-07-12 09:16:29 +0700

    feat: add searchPartner UI

---
+++ CTV-MODULE-CMS-FRONTEND/Controllers/SearchPartnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CTV_MODULE_CMS_FRONTEND.Controllers
+{
+	public class SearchPartnerController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- OcdServiceMono.API/Migrations/20240710084615_InitDB.Designer.cs
+++ OcdServiceMono.API/Migrations/20240710084615_InitDB.Designer.cs
@@ -12,7 +12,7 @@ using OcdServiceMono.API.Infrastructure.DbContexts;
 namespace OcdServiceMono.API.Migrations
 {
     [DbContext(typeof(ReadDomainDbContext))]
-    [Migration("20240709014711_InitDB")]
+    [Migration("20240710084615_InitDB")]
     partial class InitDB
     {
         /// <inheritdoc />
